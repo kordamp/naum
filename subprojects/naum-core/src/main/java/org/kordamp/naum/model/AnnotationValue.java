8d324c464c9327660111a29d4f5a96c0b9e57737
aalmiray <aalmiray@gmail.com>
2017-03-20 10:32:27 +0100

    Update license headers

--- subprojects/naum-core/src/main/java/org/kordamp/naum/model/AnnotationValue.java
+++ subprojects/naum-core/src/main/java/org/kordamp/naum/model/AnnotationValue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.kordamp.naum.model;
 
 import org.objectweb.asm.Type;
@@ -28,7 +27,9 @@ import java.util.List;
  */
 public interface AnnotationValue {
     String getType();
+
     Object getValue();
+
     String getValueAsString();
 
     static EnumValue newEnumValue(String type, String value) {
@@ -47,5 +48,4 @@ public interface AnnotationValue {
     static ArrayValue newArrayValue(List<AnnotationValue> value) {
         return new ArrayValue(value);
     }
-
 }
